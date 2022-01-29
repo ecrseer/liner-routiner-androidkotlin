c7ff15ca333f33a15d8f726e985cd69b493c4f47
ecrseer <juustino@live.com>
2022-01-29 16:58:33 -0300

    viewpager sincronizado com seekbar

--- app/src/main/java/br/infnet/dk_tp1/ui/main/MainFragment.kt
+++ app/src/main/java/br/infnet/dk_tp1/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.core.view.children
+import androidx.core.view.marginStart
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -35,30 +37,63 @@ class MainFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with (binding.seekBar as SeekBar){
+            setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    binding.viewpgr.currentItem = progress
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    //TODO("Not yet implemented")
+                }
+
+            })
+        }
+
         with(binding.viewpgr as ViewPager2){
+
             viewModel?.tarefas?.value?.size?.let{
                 adapter=SliderAdapter(childFragmentManager,lifecycle,it)
             }
-            this.children.find { it is RecyclerView }?.let {
-                ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.UP) {
-                    override fun onMove(
-                        recyclerView: RecyclerView,
-                        viewHolder: RecyclerView.ViewHolder,
-                        target: RecyclerView.ViewHolder
-                    ): Boolean {
-                        return false
-                    }
-
-                    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-
-                    }
-                }).attachToRecyclerView(it as RecyclerView)
-
-            }
+            setupItemTouchPraCima(this)
+            this.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.seekBar.progress = position
+                }
+            })
         }
-        //val pi = ProgressItem()
 
     }
+
+    private fun setupItemTouchPraCima(viewPager2: ViewPager2) {
+        viewPager2.children.find { it is RecyclerView }?.let {
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.UP) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    //todo deletar
+                }
+            }).attachToRecyclerView(it as RecyclerView)
+
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/br/infnet/dk_tp1/ui/main/SliderAdapter.kt
+++ app/src/main/java/br/infnet/dk_tp1/ui/main/SliderAdapter.kt
@@ -3,38 +3,54 @@ package br.infnet.dk_tp1.ui.main
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import br.infnet.dk_tp1.ui.tarefa.TarefaFragment
 
 
-class SliderAdapter (fa: FragmentManager, lifecycl: Lifecycle,
-                         var tamanho:Int?) : FragmentStateAdapter(fa,lifecycl) {
-        override fun getItemCount(): Int = tamanho?: 2
+class SliderAdapter(
+    fa: FragmentManager, lifecycl: Lifecycle,
+    var tamanho: Int?
+) : FragmentStateAdapter(fa,lifecycl) {
+    override
+    fun getItemCount(): Int = tamanho ?: 2
 
-        fun mudaTamanho(novo:Int){
-            if(tamanho!=novo){
-                tamanho = novo;
-                notifyDataSetChanged()
-            }
+    override
+    fun createFragment(position: Int): Fragment {
+        return tarefaPelaPosicao(position);
+    }
+
+    //override
+    fun getPageWidth(position: Int): Float {
+        return 0.9f
+    }
+
+    fun mudaTamanho(novo: Int) {
+        if (tamanho != novo) {
+            tamanho = novo;
+            notifyDataSetChanged()
         }
-        private fun tarefaPelaPosicao(position: Int): Fragment {
-            val frag = TarefaFragment()
-            frag.arguments = bundleOf("posicaoTarefaSelecionada" to position)
-            return frag;
-        }
+    }
+
+    private fun tarefaPelaPosicao(position: Int): Fragment {
+        val frag = TarefaFragment()
+        frag.arguments = bundleOf("posicaoTarefaSelecionada" to position)
+        return frag;
+    }
 
 
-        override fun createFragment(position: Int): Fragment {
-            return tarefaPelaPosicao(position);
-        }
+    //override
+    fun getCount(): Int = getItemCount()
 
-        override fun getItemId(position: Int): Long {
-            return super.getItemId(position)
-        }
+    //override
+    fun getItem(position: Int): Fragment {
+        return tarefaPelaPosicao(position);
+    }
 
-
-
+    override fun getItemId(position: Int): Long {
+        return super.getItemId(position)
+    }
 
 
 }
