3c4310a7e43f278feb9aeb007272252818c4748e
ecrseer <juustino@live.com>
2022-03-06 20:52:15 -0300

    workflowged

--- app/src/main/java/br/infnet/dk_tp1/domain/PopulateDatabase.kt
+++ app/src/main/java/br/infnet/dk_tp1/domain/PopulateDatabase.kt
@@ -2,19 +2,26 @@ package br.infnet.dk_tp1.domain
 
 object PopulateDatabase {
     val CONST_HORARIOS =  listOf(
-        Horario(1, 7, 8),
-        Horario(2, 8, 9),
-        Horario(3, 10, 11),
-        Horario(4, 11, 12),
-        Horario(5, 13, 14),
-        Horario(6, 15, 16),
+        Horario(0, 7, 8),
+        Horario(0, 8, 9),
+        Horario(0, 9, 10),
+        Horario(0, 10, 11),
+        Horario(0, 11, 12),
+        Horario(0, 12, 13),
 
-        Horario(7, 16, 17),
-        Horario(8, 17, 18),
-        Horario(9, 19, 20),
-        Horario(10, 21, 22),
-        Horario(11, 22, 23),
-        Horario(12, 23, 24)        )
+        Horario(0, 13, 14),
+        Horario(0, 14, 15),
+        Horario(0, 15, 16),
+
+        Horario(0, 16, 17),
+        Horario(0, 17, 18),
+        Horario(0, 17, 18),
+        Horario(0, 19, 20),
+
+        Horario(0, 20, 21),
+        Horario(0, 21, 22),
+        Horario(0, 22, 23),
+        Horario(0, 23, 24)        )
     val CONST_TAREFAS = listOf(
         Tarefa(1, "", "", 1),
         Tarefa(2, "", "", 2),
