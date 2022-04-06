7ce6cb759eeacb2f98b6a4cfc318ff50852844a7
ecrseer <juustino@live.com>
2022-04-06 19:48:20 -0300

    two clients mitigation

--- app/src/main/java/br/infnet/dk_tp1/ui/login/LoginFragment.kt
+++ app/src/main/java/br/infnet/dk_tp1/ui/login/LoginFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import br.infnet.dk_tp1.R
 import br.infnet.dk_tp1.databinding.FragmentLoginBinding
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,11 +28,35 @@ class LoginFragment : Fragment() {
 
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var facebookCallbackMan:CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
+        }
+    }
+    private fun setupFacb(){
+
+        with(binding.facebookLoginButton) {
+            setPermissions("email", "public_profile")
+            registerCallback(facebookCallbackMan,
+                object : FacebookCallback<LoginResult> {
+
+                    override fun onSuccess(loginResult: LoginResult) {
+
+                        //handleFacebookAccessToken(loginResult.accessToken)
+                    }
+
+                    override fun onCancel() {
+                        //Log.d(TAG, "facebook:onCancel")
+                    }
+
+                    override fun onError(error: FacebookException) {
+
+                    }
+                })
+
         }
     }
 
@@ -38,6 +66,12 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater,container,false)
+        facebookCallbackMan = CallbackManager.Factory.create()
+
+        //binding.facebookLoginButton.setReadPermissions("email","public_profile")
+
+
+
         return binding.root
         //return inflater.inflate(R.layout.fragment_login, container, false)
     }
@@ -49,6 +83,7 @@ class LoginFragment : Fragment() {
         binding.mainLoginBtn.setOnClickListener {
             SignUpDialog().show(childFragmentManager, "Criar")
         }
+        setupFacb()
     }
 
     companion object {
